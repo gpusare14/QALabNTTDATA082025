9d88e059b36a4b8e9f034cebf152a7b710eca98d
gpusare <pusare_geraldine14@outlook.com>
2025-10-01 21:57:54 -0500

    Commit a la carpeta POO_Game

---
+++ POO_Game/src/Hyundai.java
@@ -0,0 +1,11 @@
+public class Hyundai extends Auto{
+
+    Hyundai(String sPlaca){
+        //Inicializando
+        super(sPlaca);
+        super.setdVelocidadMaxima(200);
+
+        // Clase creada por Geraldine Pusare G.
+
+    }
+}
